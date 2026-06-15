32ef89c48dd15951190e92540ea71e1717b028c3
Computhink-CS127 <cs127@computhink.com.sg>
2026-06-15 03:56:52 +0000

    Jun 15, 2026, 3:56 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,40 +1,54 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
+#print("hello from day1")
 
 ########################################################################
 # Task 1:
-
-
+#print("hello")
 
 ########################################################################
 # Task 2:
-
-
-
+#print("my name is keyang")
+#print("i am 9 years old")
+#print("my school is YZPS")
+#print("i will buy Minecraft")
 ########################################################################
 # Task 3:
-
-
-
+animal1="bird"
+animal2="fish"
+animal3="cat"
+#print(animal1)
+#print(animal2)
+#print(animal3)
 ########################################################################
 # Task 4:
-
+num1=8
+num2=1234
+#print(num1 + num2)
+#print(num1 - num2)
 
 
 ########################################################################
 # Task 5:
+name="Cow"
+food="grass"
 
-
-
+#print("my name is "+ name )
+#print("my food is "+ food)
 ########################################################################
 # Task 6:
-
-
+#def intro():
+    #print("my name is keyang")
+    #print("i am 9 years old")
+    #print("my school is YZPS")
+    #print("i will buy Minecraft")
+#intro()
 
 ########################################################################
 # Task 7:
-
+def add(num1, num2):
+    print(num1 + num2)
+add(273517627675238723, 9637370293727283872)
 
 
 ########################################################################
