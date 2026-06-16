aea64e2db0ac5c56f18542a2029a57d2bad80e9f
Computhink-CS127 <cs127@computhink.com.sg>
2026-06-16 04:31:34 +0000

    Jun 16, 2026, 4:31 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -13,7 +13,7 @@ print(title + name + " has the most " + item)
 # Task 2:
 name = keyang
 num_pens = 30
-print(name + "bought" + str())
+print(name + "bought" + str(num_pens) + pens)
 
 ########################################################################
 # Task 3:
