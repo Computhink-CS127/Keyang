ab1c2b1d9bfc19d58149bc7b2986569456e3ff63
Computhink-CS127 <cs127@computhink.com.sg>
2026-06-16 04:13:13 +0000

    Jun 16, 2026, 4:13 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -7,7 +7,7 @@ print("hello from day3")
 name = input("what is your name?")
 title = input("what is your title?")
 item = input("what items you have?")
-print(title + name + "")
+print(title + name + "has the most" + item)
 
 ########################################################################
 # Task 2:
