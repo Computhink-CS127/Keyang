c01fcdbafc05eaf34145a8fab602ce4d95bee4d6
Computhink-CS127 <cs127@computhink.com.sg>
2026-06-17 02:55:47 +0000

    Jun 17, 2026, 2:55 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -64,6 +64,7 @@ import random
 diceValue1 = random.randint(1,101)
 diceValue2 = random.randint(1,101)
 
+
 ans = input(str(diceValue1) + "+" + str(diceValue2))
 
 if ans == int(diceValue1) + int(diceValue2):
