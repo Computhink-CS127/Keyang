cd9efefa5aa7e2cc6d50b7a4f695b45f7a52e227
Computhink-CS127 <cs127@computhink.com.sg>
2026-06-17 01:43:28 +0000

    Jun 17, 2026, 1:43 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -22,7 +22,7 @@ num2 = input("  ")
 print(int(num1) + int(num2))
 print(int(num1) - int(num2))
 print(int(num1) * int(num2))
-print(int(num1) + int(num2))
+print(int(num1) / int(num2))
 ########################################################################
 # Task 4:
 
